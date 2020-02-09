7d49c83c1a98ac1280303098d8d3547b9f2261a0
DrAndri <andrithorhalls@gmail.com>
2020-02-09 21:46:25 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,93 @@
 import React from 'react';
-import logo from './logo.svg';
+import Countdown from 'react-countdown';
+import Slider from "react-slick";
+import { isMobile } from "react-device-detect";
+import hildur1 from "./hildur1.jpg";
+import hildur2 from "./hildur2.jpg";
+import hildur3 from "./hildur3.jpg";
+import hildur4 from "./hildur4.jpg";
+import hildur5 from "./hildur5.jpg";
+import hildur6 from "./hildur6.jpg";
+import hildur7 from "./hildur7.jpg";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 import './App.css';
 
 function App() {
+  let now = () => {
+    let date = new Date();
+    let utc =  Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(),
+                            date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds());
+    return utc;
+  };
+  let hildurLendir = new Date(Date.UTC(2020, 3, 2, 22, 30, 0, 0));
+  let hildurLoftid = new Date(Date.UTC(2020, 3, 2, 20, 5, 0, 0));
+  const renderer = ({ total, days, hours, minutes, seconds, completed }) => {
+    if (completed) {
+      // Render a completed state
+      return "Þú ert á leiðinni!";
+    } else {
+      // Render a countdown
+      return <span>{days} dagar {hours} klukkustundir {minutes} mínútur og {seconds} sekúndur</span>;
+    }
+  };
+  var sliderSettings = {
+      dots: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: isMobile ? 1:2,
+      slidesToScroll: 1,
+      arrows: false,
+      autoplay: true
+    };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="heart love1"></div>
+      <div class="heart love2"></div>
+
+      <div class="heart love1 ver1"></div>
+      <div class="heart love2 ver1"></div>
+
+      <div class="heart love1 ver2"></div>
+      <div class="heart love2 ver2"></div>
+
+      <div class="heart love1 ver3"></div>
+      <div class="heart love2 ver3"></div>
+
+      <div class="heart love1 ver4"></div>
+      <div class="heart love2 ver4"></div>
+      <Slider {...sliderSettings}>
+        <div>
+          <img src={hildur1}/>
+        </div>
+        <div>
+          <img src={hildur2}/>
+        </div>
+        <div>
+          <img src={hildur3}/>
+        </div>
+        <div>
+          <img src={hildur4}/>
+        </div>
+        <div>
+          <img src={hildur5}/>
+        </div>
+        <div>
+          <img src={hildur6}/>
+        </div>
+        <div>
+          <img src={hildur7}/>
+        </div>
+      </Slider>
+      <div id={'countdown'}>
+        <span>Hildur lendir á Íslandi eftir:</span>
+        <br/>
+        <Countdown
+          date={hildurLendir}
+          renderer={renderer}
+          now={now} />
+      </div>
     </div>
   );
 }
